7a9188528a5c21fa1bd3c150fb9c234ff42907f4
Ghin_MLo7 <98134320+MLo7Ghinsan@users.noreply.github.com>
2024-05-14 21:36:49 -0500

    remove 32bit installer

--- setup.bat
+++ setup.bat
@@ -1,7 +1,6 @@
 @echo off
 
-echo Downloading python installer(s)
-curl -o %cd%\assets\python-3.10.11.exe https://www.python.org/ftp/python/3.10.11/python-3.10.11.exe
+echo Downloading python installer
 curl -o %cd%\assets\python-3.10.11-amd64.exe https://www.python.org/ftp/python/3.10.11/python-3.10.11-amd64.exe
 
 echo Granting write permissions to current user's directory... (for installing python)
@@ -9,19 +8,8 @@ set dir=.
 for /f "tokens=*" %%a in ('whoami') do set current=%%a
 icacls "%dir%" /grant "%current%:(oi)(ci)f" /t
 
-set "arch="
-for /f "tokens=2 delims==" %%a in ('wmic os get OSArchitecture /value') do set "arch=%%a"
-if "%arch%"=="32-bit" (
-    echo Installing python....
-    assets\python-3.10.11.exe InstallAllUsers=0 DefaultJustForMeTargetDir=%cd%\python
-    echo Python installed successfully!
-) else if "%arch%"=="64-bit" (
-    echo Installing python....
-    assets\python-3.10.11-amd64.exe InstallAllUsers=0 DefaultJustForMeTargetDir=%cd%\python
-    echo Python installed successfully!
-) else (
-    echo Unable to determine system architecture. Your system might not be compatible with python.
-)
+echo Installing python....
+assets\python-3.10.11-amd64.exe InstallAllUsers=0 DefaultJustForMeTargetDir=%cd%\python
 
 %cd%\python\python.exe -m pip install --upgrade pip --no-warn-script-location
 %cd%\python\Scripts\pip install customtkinter tk tdqm pyyaml requests pillow --no-warn-script-location
