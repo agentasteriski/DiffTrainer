8841ef623307e4c2b574d77c58b80097bc93112c
Ghin_MLo7 <98134320+MLo7Ghinsan@users.noreply.github.com>
2024-05-14 21:51:15 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,18 +1,15 @@
 @echo off
 
-echo Downloading python installer
-curl -o %cd%\assets\python-3.10.11-amd64.exe https://www.python.org/ftp/python/3.10.11/python-3.10.11-amd64.exe
+if exist "%cd%\python" (
+    set "pip_exe=%cd%\python\Scripts\pip"
+    set "python_exe=%cd%\python\python.exe"
+) else (
+    set "pip_exe=pip"
+    set "python_exe=python"
+)
 
-echo Granting write permissions to current user's directory... (for installing python)
-set dir=.
-for /f "tokens=*" %%a in ('whoami') do set current=%%a
-icacls "%dir%" /grant "%current%:(oi)(ci)f" /t
-
-echo Installing python....
-assets\python-3.10.11-amd64.exe /simple AssociateFiles=0 Shortcuts=0 Include_doc=0 Include_launcher=0 InstallAllUsers=0 DefaultJustForMeTargetDir=%cd%\python
-
-%cd%\python\python.exe -m pip install --upgrade pip --no-warn-script-location
-%cd%\python\Scripts\pip install customtkinter tk tdqm pyyaml requests pillow --no-warn-script-location
+%python_exe% -m pip install --upgrade pip --no-warn-script-location
+%pip_exe% install customtkinter tk tdqm pyyaml requests pillow --no-warn-script-location
 
 echo Setup complete!
 echo Launching gui...
