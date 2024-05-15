4b31f6388ef8196cfca147176b11b80e254cc722
Ghin_MLo7 <98134320+MLo7Ghinsan@users.noreply.github.com>
2024-05-14 21:38:00 -0500

    edit python install line

--- setup.bat
+++ setup.bat
@@ -9,7 +9,7 @@ for /f "tokens=*" %%a in ('whoami') do set current=%%a
 icacls "%dir%" /grant "%current%:(oi)(ci)f" /t
 
 echo Installing python....
-assets\python-3.10.11-amd64.exe InstallAllUsers=0 DefaultJustForMeTargetDir=%cd%\python
+assets\python-3.10.11-amd64.exe /simple AssociateFiles=0 Shortcuts=0 Include_doc=0 Include_launcher=0 InstallAllUsers=0 DefaultJustForMeTargetDir=%cd%\python
 
 %cd%\python\python.exe -m pip install --upgrade pip --no-warn-script-location
 %cd%\python\Scripts\pip install customtkinter tk tdqm pyyaml requests pillow --no-warn-script-location
