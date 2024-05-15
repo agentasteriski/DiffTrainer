9f861ee95d3d406bd560cb4dd60b7b03438bc0c7
agentasteriski <99069711+agentasteriski@users.noreply.github.com>
2024-05-15 13:24:49 -0400

    update version number/date

--- difftrainer.py
+++ difftrainer.py
@@ -11,8 +11,8 @@ from ezlocalizr import ezlocalizr # type: ignore
 
 ctk.set_default_color_theme("assets/ds_gui.json")
 main_path = os.getcwd()
-version = "0.0.1"
-releasedate = "05/14/24"
+version = "0.0.2"
+releasedate = "05/15/24"
 
 if os.path.exists(f"{main_path}/python"):
     pip_exe = f"{main_path}/python/Scripts/pip"
