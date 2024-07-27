4cd79eb3c92020c5db2683b889624b168426ca0f
agentasteriski <99069711+agentasteriski@users.noreply.github.com>
2024-07-27 16:09:58 -0400

    Add files via upload

--- conda_installer.bat
+++ conda_installer.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo Downloading Miniconda installer
-curl -o %cd%\assets\Miniconda3-latest-Windows-x86_64.exe https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
+curl -o %cd%\assets\Miniconda3-py310_24.5.0-0-Windows-x86_64.exe https://repo.anaconda.com/miniconda/Miniconda3-py310_24.5.0-0-Windows-x86_64.exe
 
 echo Granting write permissions to current user's directory... (for installing python)
 set dir=.
@@ -9,6 +9,6 @@ for /f "tokens=*" %%a in ('whoami') do set current=%%a
 icacls "%dir%" /grant "%current%:(oi)(ci)f" /t
 
 echo Installing Miniconda....
-assets\Miniconda3-latest-Windows-x86_64.exe /S /D=%cd%\python
+assets\Miniconda3-py310_24.5.0-0-Windows-x86_64.exe /S /D=%cd%\python
 
 pause
