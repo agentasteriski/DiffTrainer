86bdf968b86a67f71b52367c6befa7272aeac306
agentasteriski <99069711+agentasteriski@users.noreply.github.com>
2024-09-19 01:09:31 -0400

    Update run_quickinference.bat

--- run_quickinference.bat
+++ run_quickinference.bat
@@ -1,22 +1,11 @@
 @echo off
 
-if exist "%cd%\miniconda" (
-	set "conda_hook=%cd%\condabin\conda_hook.bat"
-) else if exist "C:\Users\%username%\anaconda3" (
-	set "conda_hook=C:\Users\%username%\anaconda3\condabin\conda_hook.bat"
-) else if exist "C:\Users\%username%\miniconda3" (
-	set "conda_hook=C:\Users\%username%\miniconda3\condabin\conda_hook.bat"
-) else if exist "C:\ProgramData\anaconda3" (
-	set "conda_hook=C:\ProgramData\anaconda3\condabin\conda_hook.bat"
-) else if exist "C:\ProgramData\miniconda3" (
-	set "conda_hook=C:\ProgramData\miniconda3\condabin\conda_hook.bat"
-) else (
-	echo Conda not located, proceeding anyways...
-)
+set "conda_python=%cd%\miniconda\python.exe"
 
-call %conda_hook%
-call conda activate difftrainerA
-echo Activating environment A...
-python quickinference.py
+if exist %conda_python% (
+	%conda_python% quickinference.py
+) else (
+	python quickinference.py
+)
 
 pause
